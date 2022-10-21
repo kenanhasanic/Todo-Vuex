6e8b1bce62d47e345206ca6b1006434bf71de663
kenanhasanic <kenanhasanich@gmail.com>
2022-10-21 15:57:38 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <AddTodo />
+    <FilterTodos/>
+    <TodoItems />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoItems from './components/Todos.vue'
+import AddTodo from './components/AddTodo.vue'
+import FilterTodos from './components/FilterTodos.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TodoItems,
+    AddTodo,
+    FilterTodos
   }
 }
 </script>
@@ -22,5 +30,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+  line-height: 1.6;
+  background: #e8f7f0;
+}
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createStore } from 'vuex'
+import todos from '../src/store/modules/todos'
+
+// Create a new store instance.
+const store = createStore({
+  state () {
+    return {
+      count: 0
+    }
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  },
+  modules: {
+    todos
+  }
+})
+
+const app = createApp(App)
+
+// Install the store instance as a plugin
+app.use(store)
+
+
+app.mount('#app')
